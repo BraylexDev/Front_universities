66b30efb6c5ef89f18fc3d871eaeeb87a484ce87
BrayanAlexisPerez <bperezr@unicauca.edu.co>
2024-05-06 16:14:30 -0500

    update api fake direction

--- src/app/service/apiServer.ts
+++ src/app/service/apiServer.ts
@@ -1,3 +1,4 @@
 export const apiServer = {
-    serverUrl: 'http://localhost:4200/assets/prueba/datos_prueba.json'
+    //serverUrl: 'http://localhost:4200/assets/prueba/datos_prueba.json'
+    serverUrl: 'https://top-arab-researchers.onrender.com/assets/prueba/datos_prueba.json'
 }
