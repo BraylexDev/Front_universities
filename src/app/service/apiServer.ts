e30c870c1b4846967de2a48744b7d656e6156509
BrayanAlexisPerez <bperezr@unicauca.edu.co>
2025-04-01 00:14:20 -0500

    filter was added by institution and committee members

--- src/app/service/apiServer.ts
+++ src/app/service/apiServer.ts
@@ -1,5 +1,5 @@
 export const apiServer = {
-    serverUrl: 'https://4gqt4jvr-4200.use2.devtunnels.ms/assets/prueba/datos_prueba_2025.json'
+    /* serverUrl: 'https://4gqt4jvr-4200.use2.devtunnels.ms/assets/prueba/datos_prueba_2025.json' */
     /* serverUrl: 'http://localhost:4200/assets/prueba/datos_prueba_2025_Marzo.json' */
-    /* serverUrl: 'https://top-arab-researchers.onrender.com/assets/prueba/datos_prueba_2025_Marzo.json' */
+    serverUrl: 'https://top-arab-researchers.onrender.com/assets/prueba/datos_prueba_2025_Marzo.json'
 }
